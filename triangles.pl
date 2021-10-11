7c972dfb554be81606cee277bf6b7124fb90e20f
paulo.pacitti <paulo.pacitti@ifood.com.br>
2021-10-11 15:33:17 -0300

    feat: data described. record functor done, draft inrange functor in progress

--- triangles.pl
+++ triangles.pl
@@ -2,6 +2,16 @@
 % prints the number of intersections between them and their pairs
 % Use this to run the program: swipl -q -f triangles.pl -t topo < teste
 
-topo():-
+% data
+:- dynamic ret/5.
+
+topo :-
     read(Input),       % Reads the input
+    record(Input),
     write_ln(Input).
+
+record([]).
+record([X|R]) :- assertz(X), record(R).
+
+% draft functor, not working
+inrange(X11, X12, Y11, Y12, X21, X22, Y21, Y22) :- (X11 >= X21, Y11 >= Y21), (X12 =< X22, Y12 =< Y22).
