386f988d25326ab947df88f22d1393e29f47eae0
Caio Henrique Pardal <pardalcaio@hotmail.com>
2021-10-13 21:49:09 -0300

    feat: Implements a new list_to_set function

--- triangles.pl
+++ triangles.pl
@@ -20,7 +20,7 @@ record([X|R]) :- assertz(X), record(R).
 
 % intersection(-ListOfRectangles, +ListOfRectanglesThaHaveIntersection)
 intersection([], _).
-intersection([X|R], _) :- 
+intersection([X|R], Pairs) :- 
     findall(ret(Name, X11, X12, Y11, Y12), ret(Name, X11, X12, Y11, Y12), FindResult), % get all rectangles in the database
     include(inrange(X), FindResult, InRangeList), % "filter", +InRangeList contains the names of the rectangles that intercept X
     make_pairs(X, InRangeList, Pairs), % +Pairs is the array of pairs (X, RectangleThatIntercepts)
@@ -34,16 +34,37 @@ inrange(ret(_, X11, Y11, X12, Y12), ret(_,X21, Y21, X22, Y22)) :-
 
 % not working :(, but I'm close.
 make_pairs(_, [], _).
-make_pairs(ret(Name1,X11, X12, Y11, Y12), [ret(Name2,_,_,_,_)|R], Pairs) :- 
+make_pairs(ret(Name1, X11, X12, Y11, Y12), [ret(Name2,_,_,_,_)|R], Pairs) :- 
     append(Pairs, [[Name1, Name2]], NewPairs),
-    write(NewPairs).
-    make_pairs(ret(Name1,X11, X12, Y11, Y12), R, NewPairs).
+    make_pairs(ret(Name1, X11, X12, Y11, Y12), R, NewPairs).
 
-list_to_set([],_).
-list_to_set([[X,Y]|R],Set) :- 
-    (not(member([X,Y],[[X,Y]|R])), not(member([Y,X],[[X,Y]|R]))),
-    append(Set, [X,Y], NewSet),
-    list_to_set(R, NewSet).
+%%%%%%%%%%% Helper Functions %%%%%%%%%%%
+
+% Converts a list to a set, reference code: https://www.swi-prolog.org/pldoc/man?predicate=list_to_set/2
+list_to_set(List, Set) :-
+    must_be(list, List),
+    number_list(List, 1, Numbered),
+    sort(1, @=<, Numbered, ONum),
+    remove_dup_keys(ONum, NumSet),
+    sort(2, @=<, NumSet, ONumSet),
+    pairs_keys(ONumSet, Set).
+
+number_list([], _, []).
+number_list([H|T0], N, [H-N|T]) :-
+    N1 is N+1,
+    number_list(T0, N1, T).
+
+remove_dup_keys([], []).
+remove_dup_keys([H|T0], [H|T]) :-
+    H = V-_,
+    remove_same_key(T0, V, T1),
+    remove_dup_keys(T1, T).
+
+remove_same_key([V1-_|T0], V, T) :-
+    V1 == V,
+    !,
+    remove_same_key(T0, V, T).
+remove_same_key(L, _, L).
 
 output([]).
 output([X|R]) :- writef('%1L%w\n', X), output(R).
